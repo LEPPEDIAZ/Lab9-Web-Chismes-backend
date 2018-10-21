47ccb056267e1c759e48ed24a3865afbec68d512
Unknown <dia151378@uvg.edu.gt>
2018-10-21 16:17:53 -0300

    data added by frontend

--- chismes/urls.py
+++ chismes/urls.py
@@ -18,6 +18,7 @@ from django.views.generic import TemplateView
 
 from notes import endpoints
 
+
 urlpatterns = [
     url(r'^api/', include(endpoints)),
     url(r'^', TemplateView.as_view(template_name="index.html")),
